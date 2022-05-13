3348235d9753ab5d97ae52aab3363df7fc73f4aa
hmtmcse <hmtm.cse@gmail.com>
2022-05-13 09:20:12 +0600

    working on Code generator

---
+++ boot/react/helper/icon-helper.tsx
@@ -0,0 +1,6 @@
+export const BootstrapIconHelper = {
+    ViewIcon: "bi bi-eye",
+    EditIcon: "bi bi-pencil",
+    DeleteIcon: "bi bi-trash",
+    TableRowActionIcon: "bi bi-three-dots-vertical",
+}
